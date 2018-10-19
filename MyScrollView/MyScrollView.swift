5daee40db5a95b1f16c1f1b2fe51e03b28b20142
Francisco Igor <franciscoigor@gmail.com>
2018-10-18 21:26:03 -0700

    fix bound translation

--- MyScrollView/MyScrollView.swift
+++ MyScrollView/MyScrollView.swift
@@ -8,55 +8,87 @@
 
 import UIKit
 
-@IBDesignable
+//@IBDesignable
 class MyScrollView: UIView {
     
-    @IBInspectable
-    var contentSize : CGSize = CGSize() {
-        didSet {
-            frame.size = contentSize
-        }
+    //    @IBInspectable
+    var contentSize = CGSize()
+    var maxWidth : CGFloat = 0.0
+    var maxHeight : CGFloat  = 0.0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentSize = CGSize(width: frame.width, height: frame.height)
+        addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:))))
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
+        self.clipsToBounds = true
     }
     
-   
-    override func willMove(toWindow newWindow: UIWindow?) {
-       print("View appear")
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    override func addSubview(_ view: UIView) {
+        super.addSubview(view)
+        maxWidth = max(maxWidth, view.frame.origin.x + view.frame.width + 50)
+        maxHeight = max(maxHeight, view.frame.origin.y + view.frame.height + 50)
+    }
+    
+    @objc func handleTap(recognizer:UIPanGestureRecognizer) {
+        print("Tap")
+        
+    }
+    
+    @objc func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self)
+        if let view = recognizer.view  {
+            let scrollView = view as! MyScrollView
+            scrollView.handleTranslation(translation: translation)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self)
     }
     
     func handleTranslation(translation: CGPoint){
         
-        let screenSize = UIScreen.main.bounds
+        let viewSize = self.bounds
+        
+        let originX = self.bounds.origin.x
+        let originY = self.bounds.origin.y
         
         // move using translation coordinates
-        self.center = CGPoint(x:self.center.x + translation.x,
-                              y:self.center.y + translation.y)
+        self.bounds.origin.x -= translation.x
+        self.bounds.origin.y -= translation.y
         
-        var newx = self.frame.origin.x
-        var newy = self.frame.origin.y
+        var newx = self.bounds.origin.x
+        var newy = self.bounds.origin.y
         
         // check right bound
-        if (screenSize.width - newx > contentSize.width){
-            newx = screenSize.width - self.contentSize.width
+        print("CUR", newx, viewSize.width, maxWidth)
+        if (viewSize.width + newx > maxWidth){
+            newx = self.maxWidth - viewSize.width
+            print("NEW", newx, viewSize.width, maxWidth)
         }
         // check bottom bound
-        if (screenSize.height - newy > contentSize.height){
-            newy = screenSize.height - self.contentSize.height
+        if (viewSize.height + newy > maxHeight){
+            newy = self.maxHeight - viewSize.height 
         }
         
         // check left bound
-        if (newx > 0){
+        if (newx < 0){
             newx = 0
         }
         // check top bound
-        if (newy > 0){
+        if (newy < 0){
             newy = 0
         }
         
-        UIView.animate(withDuration: 0.1, animations: {
-            self.frame.origin.x = newx
-            self.frame.origin.y = newy
-        })
+        //UIView.animate(withDuration: 0.1, animations: {
         
+        
+        self.bounds.origin.x += (newx - originX) / 2
+        self.bounds.origin.y += (newy - originY) / 2
         
     }
     
@@ -75,11 +107,11 @@ class MyScrollView: UIView {
     
     
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
 }
--- MyScrollView/MyScrollViewController.swift
+++ MyScrollView/MyScrollViewController.swift
@@ -11,66 +11,44 @@ import UIKit
 class MyScrollViewController: UIViewController , UIGestureRecognizerDelegate {
 
     
-    var gestureRecognizer: UIPanGestureRecognizer!
-    var mainView: MyScrollView!
+//    var gestureRecognizer: UIPanGestureRecognizer!
+    var scrollView: MyScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //get screen size
-        //let screenSize = UIScreen.main.bounds
         
-        //create main View
-        mainView = MyScrollView()
-        mainView.contentSize = CGSize(width: 600, height: 800)
-        mainView.backgroundColor = UIColor.init(red: 0.9, green: 0.9, blue: 0.9, alpha:1.0)
-        mainView.clipsToBounds = true
-        view.addSubview(mainView)
+        // main View
+        createMainView(color: UIColor.init(red: 0.9, green: 0.9, blue: 0.9, alpha:1.0))
+
+    }
+    
+    func createMainView( color: UIColor){
+        // create main View
+        scrollView = MyScrollView(frame: CGRect(x: 0, y:0, width: 300, height: 300))
+        scrollView.backgroundColor = color
+        view.addSubview(scrollView)
         
-        // add gesture recognizer to mainView
-        gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
-        gestureRecognizer.delegate = self
-        mainView.addGestureRecognizer(gestureRecognizer)
         
         // add subviews
-        addView(rect: CGRect(x: 20, y: 20, width: 100, height: 100) , color: .red)
-        addView(rect: CGRect(x: 150, y: 150, width: 150, height: 200) , color: .green)
-        addView(rect: CGRect(x: 40, y: 400, width: 200, height: 150) , color: .blue)
-        addView(rect: CGRect(x: 100, y: 600, width: 180, height: 150) , color: .yellow)
-        
-        
+        addScrollSubView(rect: CGRect(x: 20, y: 20, width: 100, height: 100) , color: .red)
+        addScrollSubView(rect: CGRect(x: 150, y: 150, width: 150, height: 200) , color: .green)
+        addScrollSubView(rect: CGRect(x: 40, y: 400, width: 200, height: 150) , color: .blue)
+        addScrollSubView(rect: CGRect(x: 100, y: 600, width: 180, height: 150) , color: .yellow)
         
     }
     
-    func addView(rect: CGRect, color: UIColor){
-        let subView = UIView()
-        subView.frame = rect
+    
+    func addScrollSubView(rect: CGRect, color: UIColor){
+        let subView = UIView(frame: rect)
         subView.backgroundColor = color
-        mainView.addSubview(subView)
-        
-        
+        scrollView.addSubview(subView)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+        scrollView.handleTranslation(translation: CGPoint(x: 0, y: 0))
     }
     
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
-        let translation = recognizer.translation(in: self.view)
-        if let view = recognizer.view  {
-            let scrollView = view as! MyScrollView
-            scrollView.handleTranslation(translation: translation)
-            
-        }
-        recognizer.setTranslation(CGPoint.zero, in: self.view)
-    }
 
 
 }
